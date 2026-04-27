fc9dfc255b44f38a2dafd517ebb634a21ca9b338
Ming Xu <xumingxs@qq.com>
2026-04-27 09:18:59 -0700

    impl: 移除旧markdown解析器相关源码1

--- src/syntax.rs
+++ src/syntax.rs
@@ -2,15 +2,12 @@ mod text;
 
 pub use text::{Text, TextChange};
 
-use markdown::{Constructs, ParseOptions, mdast};
+use pulldown_cmark;
 use smart_default::SmartDefault;
 use tracing::info;
-use comrak::{self, Arena};
-use comrak::nodes::{Ast, NodeValue};
 
 struct Snapshot {
     content: ropey::Rope,
-    md_ast: mdast::Node,
 }
 
 #[derive(SmartDefault)]
@@ -19,7 +16,6 @@ pub struct SyntaxServer {
     version_map: std::collections::HashMap<i32, Vec<TextChange>>,
     #[default(0)]
     cur_version: i32,
-    md_ast: Option<mdast::Node>,
     snapshots_idx: std::collections::VecDeque<i32>,
     snapshots: std::collections::HashMap<i32, Snapshot>,
 }
@@ -32,14 +28,14 @@ impl SyntaxServer {
         self.version_map.insert(version, changes);
     }
 
-    fn snapshot_push(&mut self, content: ropey::Rope, md_ast: mdast::Node) {
-        if self.snapshots_idx.len() >= Self::SNAPSHOT_INTERVAL
-            && let Some(oldest) = self.snapshots_idx.pop_front() {
-                self.snapshots.remove(&oldest);
-            }
-        let snapshot = Snapshot { content, md_ast };
-        self.snapshots.insert(self.cur_version, snapshot);
+    pub fn take_snapshot(&mut self) {
         self.snapshots_idx.push_back(self.cur_version);
+        self.snapshots.insert(
+            self.cur_version,
+            Snapshot {
+                content: self.text.get_content().clone(),
+            },
+        );
     }
 
     fn snapshot_get(&self, version: i32) -> Option<&Snapshot> {
@@ -67,35 +63,9 @@ impl SyntaxServer {
         self.text.refresh(new_content);
         self.version_map.clear();
         self.cur_version = 0;
-
-        let parse_options = ParseOptions {
-            constructs: Constructs {
-                math_text: true,
-                math_flow: true,
-                ..Default::default()
-            },
-            math_text_single_dollar: true,
-            ..Default::default()
-        };
-
-        
-
-        self.md_ast = markdown::to_mdast(&self.text.get_string(), &parse_options).ok();
-
-        if let Some(md_ast) = &self.md_ast {
-            self.snapshot_push(self.text.get_content().clone(), md_ast.clone());
-            self.info_ast();
-            info!("initial snapshot created for version 0");
-        }
     }
 
-    pub fn info_ast(&self) {
-        if let Some(md_ast) = &self.md_ast {
-            info!("current AST: {:#?}", md_ast);
-        } else {
-            info!("no AST available");
-        }
-    }
+    pub fn info_ast(&self) {}
 
     pub fn get_string(&self) -> String {
         self.text.get_string()
